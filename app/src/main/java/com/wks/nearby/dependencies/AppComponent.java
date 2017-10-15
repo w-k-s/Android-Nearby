43e7950d6ca28bf4e2239ea170432859bd1c876c
Waqqas Sheikh <waqqas.abdulkareem@gmail.com>
2017-10-15 23:54:14 +0400

    - BAH! Made a stupid mistake. Resurrecting the Picasso module.

--- app/src/main/java/com/wks/nearby/dependencies/AppComponent.java
+++ app/src/main/java/com/wks/nearby/dependencies/AppComponent.java
@@ -2,8 +2,10 @@ package com.wks.nearby.dependencies;
 
 import android.content.Context;
 
+import com.squareup.picasso.Picasso;
 import com.wks.nearby.data.places.source.PlacesRepository;
 import com.wks.nearby.dependencies.modules.AppModule;
+import com.wks.nearby.dependencies.modules.PicassoModule;
 import com.wks.nearby.dependencies.modules.repositories.places.PlacesRepositoryModule;
 
 import dagger.Component;
@@ -12,10 +14,12 @@ import dagger.Component;
  * Created by waqqassheikh on 13/10/2017.
  */
 @AppScope
-@Component(modules = {AppModule.class, PlacesRepositoryModule.class})
+@Component(modules = {AppModule.class, PicassoModule.class, PlacesRepositoryModule.class})
 public interface AppComponent {
 
     Context getContext();
 
     PlacesRepository getPlacesRepository();
+
+    Picasso getPicasso();
 }
--- app/src/main/java/com/wks/nearby/utils/BindingAdapters.java
+++ app/src/main/java/com/wks/nearby/utils/BindingAdapters.java
@@ -5,6 +5,7 @@ import android.text.TextUtils;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.wks.nearby.app.App;
 
 /**
  * Created by waqqassheikh on 15/10/2017.
@@ -15,6 +16,9 @@ public class BindingAdapters {
     @BindingAdapter("imageUrl")
     public static void setImageUrl(ImageView imageView, String url) {
         if (TextUtils.isEmpty(url)) return;
-        Picasso.with(imageView.getContext()).load(url).into(imageView);
+        final Picasso picasso = ((App)imageView.getContext().getApplicationContext()).getComponent().getPicasso();
+        if (picasso != null) {
+            picasso.with(imageView.getContext()).load(url).into(imageView);
+        }
     }
 }
